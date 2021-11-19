1fd62baafdaa587949a3f145250eb98a9fa336bb
wakishi <hun73r@icloud.com>
2021-11-19 22:06:31 +0100

    add files

---
+++ src/components/RecipientsDisplay.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let recipients
+
+  let emailList = recipients;
+  var mailWidth = document.getElementById("mails");
+  
+  const containerWidth = document.getElementById("mails");
+  
+  // for (let i = recipients.length; i > 0; i--) {
+  //   if (mailWidth < )
+    
+  // }
+  // console.log(emailList);
+  console.log(mailWidth);
+</script>
+
+<span id="mails">{recipients}</span>
